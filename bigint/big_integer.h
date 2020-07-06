ff26cad7ff580c707b5896fb06d20045022aa90f
Stepavly <mentos092092@gmail.com>
2020-07-07 00:46:48 +0300

    Prefinal commit

--- bigint/big_integer.h
+++ bigint/big_integer.h
@@ -10,7 +10,7 @@ struct big_integer {
 	big_integer(int a);
 	explicit big_integer(const std::string &str);
 	big_integer(uint32_t a);
-	~big_integer();
+	~big_integer() = default;
 
 	big_integer &operator=(big_integer const &other);
 
@@ -59,11 +59,73 @@ struct big_integer {
 
 	friend std::string to_string(big_integer a);
 
+	bool positive() const;
+	bool is_zero() const;
+
  private:
 	big_integer(bool sign, std::vector<uint32_t> digits);
 	friend int compare(const big_integer &a, const big_integer &b);
-	bool positive() const;
+	uint32_t operator[](size_t index) const {
+		return dig[index];
+	}
+	uint32_t &operator[](size_t index) {
+		return dig[index];
+	}
+	size_t size() const {
+		return dig.size();
+	}
 	void normalize();
+
+	template<class BitFunction>
+	friend big_integer bit_function_applier(big_integer lhs,
+	                                        big_integer rhs,
+	                                        BitFunction const &bit_function) {
+		size_t result_len = std::max(lhs.size(), rhs.size()) + 1;
+
+		if (!lhs.sign) {
+			++lhs;
+			lhs.dig.resize(result_len, 0u);
+
+			for (uint32_t &x : lhs.dig) {
+				x = ~x;
+			}
+		} else {
+			lhs.dig.resize(result_len, 0u);
+		}
+
+		if (!rhs.sign) {
+			++rhs;
+			rhs.dig.resize(result_len, 0u);
+
+			for (uint32_t &x : rhs.dig) {
+				x = ~x;
+			}
+		} else {
+			rhs.dig.resize(result_len, 0u);
+		}
+
+		std::vector<uint32_t> result_num(result_len);
+		bool result_sign = !bit_function(!lhs.sign, !rhs.sign);
+
+		for (size_t i = 0; i < result_len; i++) {
+			uint32_t a = i < lhs.size() ? lhs[i] : 0;
+			uint32_t b = i < rhs.size() ? rhs[i] : 0;
+			uint32_t c = bit_function(a, b);
+
+			if (!result_sign) {
+				c = ~c;
+			}
+
+			result_num[i] = c;
+		}
+
+		big_integer result = big_integer(result_sign, result_num);
+
+		return result_sign ? result : --result;
+	}
+
+	std::pair<big_integer, uint32_t> div_mod_short(uint32_t rhs);
+	big_integer div_long(big_integer const &rhs);
 
 	bool sign;
 	std::vector<uint32_t> dig;
